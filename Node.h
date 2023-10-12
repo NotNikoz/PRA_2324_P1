9b3649014b4bd2735ad108369460363c05f440cf
Nikos Loizou Fernandez <nloifer@alumno.upv.es>
2023-10-12 17:50:25 +0200

    Añadido código de test de la clase Node; Makefile actualizado

--- Node.h
+++ Node.h
@@ -5,8 +5,16 @@ template <typename T>
 class Node {
 	public :
 		T data = 0; //El elemento almacenado de tipo genérico T  
-		Node <T>* next = 0; //Puntero al siguiente nodo de la secuencia
-		Node (T data, Node <T>* next = nullptr) = 0; //Método constructor next será null en caso de que no se propocione
-		friend ostream& operator<<(ostream &out, const Node <T> &node) = 0; //Sobrecarga del operador << para imprimir una instancia de Node
+		Node <T>* next; //Puntero al siguiente nodo de la secuencia
+		Node (T data, Node <T>* next = nullptr) {
+		       	//Método constructor next será null en caso de que no se propocione
+			this -> data = data;
+			this -> next = next;
+		}
+		friend ostream& operator<<(ostream &out, const Node <T> &node) {
+		       	//Sobrecarga del operador << para imprimir una instancia de Node
+			out << node.data;
+			return out;
+		}
 
 };
